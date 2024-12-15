656013515267cac9b083e05f5634206f10b4efa7
wota-n <najmeenajib99@gmail.com>
2024-12-15 07:56:12 +0000

    add new variables for cluster and node

---
+++ proxmox-module/variables.tf
@@ -0,0 +1,38 @@
+variable "vm_name" {
+  default = null
+}
+
+variable "node_name" {
+  default = null
+}
+
+variable "pm_api_url" {}
+variable "pm_api_token_id" {}
+variable "pm_api_token_secret" {}
+
+variable "cluster" {
+  description = "Cluster config"
+  type = object({
+    name = string
+    endpoint = string
+    gateway = string
+    talos_version = string
+    proxmox_cluster = string 
+  })
+}
+
+variable "nodes" {
+  description = "Configuration for cluster nodes"
+  type = map(object({
+    host_node     = string
+    machine_type  = string
+    datastore_id = optional(string, "local-zfs")
+    ip            = string
+    mac_address   = string
+    vm_id         = number
+    cpu           = number
+    ram_dedicated = number
+    update = optional(bool, false)
+    igpu = optional(bool, false)
+  }))
+}
